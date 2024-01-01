add64e2fcfcfd5a6f320e6afb0072b6c9ea41944
Jurian <jurian@unafix.com>
2024-01-01 22:03:33 +0100

    fixed a treesitter config bug

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -10,7 +10,7 @@ require 'nvim-treesitter.configs'.setup {
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = true,
-    }
+    },
 
     autotag = {
         enable = true,
