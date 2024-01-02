1767c0a2dfe163c84b9d86f12afab329c636d5fc
Jurian <jurian@unafix.com>
2024-01-02 17:38:43 +0100

    added in ctags, use C-]

--- lua/jurian/packer.lua
+++ lua/jurian/packer.lua
@@ -68,6 +68,7 @@ return require('packer').startup(function(use)
 
     use{'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}   -- Treesitter parser
     use{'nvim-treesitter/nvim-treesitter-context'}
+    use{'ludovicchabant/vim-gutentags'}                         -- Gutentags CTags
     use('theprimeagen/harpoon')                                 -- Harpoon bookmarking
     use{'codota/tabnine-nvim', run = './dl_binaries.sh'}        -- Tabnine auto-completion
     use{'windwp/nvim-ts-autotag'}				-- Autotag tag-completion
