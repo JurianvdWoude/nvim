d80d17e9c292896b97f114940a80c499103b87cc
Jurian <jurian@unafix.com>
2024-01-18 23:36:16 +0100

    added in lua snippets

--- lua/jurian/packer.lua
+++ lua/jurian/packer.lua
@@ -39,6 +39,7 @@ return require('packer').startup(function(use)
             {'hrsh7th/cmp-path'},
             {'hrsh7th/cmp-nvim-lsp'},
             {'hrsh7th/cmp-nvim-lua'},
+            {'saadparwaiz1/cmp_luasnip'},
         }
     }
 
