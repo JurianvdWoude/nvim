b843cdefb6cde2c4c3ffa153119b4560520ef8ab
Blazeye <jurianvanderwoude@gmail.com>
2024-11-15 17:50:36 +0100

    added in tmux pane switching

--- init.lua
+++ init.lua
@@ -1,2 +1,43 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+--[[
+-- Show relative numbers to your position.
+vim.opt.relativenumber = true
+
+-- Set the the key which is used in special key combinations for hotkeys.
+vim.g.mapleader = " "
+
+-- S E T T I N G S
+-- The number of spaces that a tab in the file counts for.
+vim.opt.tabstop = 4
+
+-- The number of spaces that a tab in the file counts for when editing.
+vim.opt.softtabstop = 4
+
+-- The number of spaces to use for (auto)indent.
+vim.opt.shiftwidth = 4
+
+-- Use the appropriate number of spaces to insert a tab.
+vim.opt.expandtab = true
+
+-- Set the colorscheme
+-- vim.opt.color = "evening"
+
+-- Add numbers to each line on the left-hand side.
+vim.opt.number = true
+
+-- Highlight cursor line underneath the cursor horizontally.
+vim.opt.cursorline = true
+
+-- Never have less than 4 columns below or above your cursor
+vim.opt.scrolloff = 4
+
+-- R E M A P S
+-- Move to the filesystem.
+vim.keymap.set("n", "<leader>fs", vim.cmd.Ex)
+
+-- Center the cursor after scrolling up or down.
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+]]
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<C-h>", "<cmd>!tmux select-pane -L<cr>", { desc = "Move to Left Tmux Pane", remap = true })
+vim.keymap.set("n", "<C-j>", "<cmd>!tmux select-pane -D<cr>", { desc = "Move to Lower Tmux Pane", remap = true })
+vim.keymap.set("n", "<C-k>", "<cmd>!tmux select-pane -U<cr>", { desc = "Move to Upper Tmux Pane", remap = true })
+vim.keymap.set("n", "<C-l>", "<cmd>!tmux select-pane -R<cr>", { desc = "Move to Right Tmux Pane", remap = true })
+
+vim.keymap.set("n", "<S-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+vim.keymap.set("n", "<S-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+vim.keymap.set("n", "<S-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+vim.keymap.set("n", "<S-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
